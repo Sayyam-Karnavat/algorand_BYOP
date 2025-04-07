4808bcbf88a473629846f32cc2022e855a0a007b
Sayyam-Karnavat <sanyam.karnavat5@gmail.com>
2025-04-07 13:50:37 +0530

    Added the Main function to call the arc19 class

--- arc19.py
+++ arc19.py
@@ -139,3 +139,35 @@ class ARC19:
         print("Transaction sent :- {}".format(tx_id))
 
         return tx_id
+    
+
+if __name__ == "__main__":
+
+    arc_obj = ARC19()
+
+
+    # CID is basically the IPFS file hash 
+    cid = arc_obj.upload_metadata(file_path="temp.pdf")
+
+    if cid:
+        name = "Blockchain PDF"
+        desc = "Research paper blockchain"
+
+        metadata_hash = arc_obj.create_metadata(
+            asset_name=name,
+            description=desc,
+            ipfs_hash=cid
+        )
+
+        reserve_address = arc_obj.reserve_address_from_cid(cid=cid)
+
+        url = arc_obj.create_url_from_cid(cid=cid)
+
+        transaction_id = arc_obj.create_asset(
+            metadata_hash=metadata_hash,
+            reserve_address=reserve_address,
+            url=url
+        )
+
+    else:
+        print("CID is empty :( ")
